e5c21660db9336f376fa1fcce38b95f1fce70cbb
ankitkumar810 <ankit5456kumar@gmail.com>
2026-06-29 14:09:31 +0530

    JavaScript Learning

---
+++ CSS_Marquee_TrainX/JavaScript/Script.js
@@ -0,0 +1,16 @@
+alert("Holla")
+
+var name ='Ankit'
+var address = 'Vadodara'
+console.group();
+console.log('Normal Text');
+console.warn('Normal Text')
+console.error('Normal Text');
+console.groupEnd();
+
+
+
+function greet(){
+console.warn('Good Afternoon');
+}
+greet();
