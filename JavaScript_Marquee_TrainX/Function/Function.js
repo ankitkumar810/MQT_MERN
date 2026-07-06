f7cf99cb364a68dff08c2cddcd23906bd0222f28
ankitkumar810 <ankit5456kumar@gmail.com>
2026-07-06 11:31:33 +0530

    Functions

---
+++ JavaScript_Marquee_TrainX/Function/Function.js
@@ -0,0 +1,15 @@
+function greet(name){
+    return `Hello, ${name}!`;
+}
+
+let greetMsg = greet('Ankit Kumar')
+console.log(greetMsg);
+
+ greetMsg = greet('Aakash Kumar')
+console.log(greetMsg);
+
+ greetMsg = greet('Vinay')
+console.log(greetMsg);
+
+ greetMsg = greet('Aakash Kumar')
+console.log(greetMsg);
---
+++ JavaScript_Marquee_TrainX/Function/fn.js
@@ -0,0 +1,24 @@
+//function can be passed as an argument too
+// function sayHello(name){
+//     return `Hello, ${name}!`;
+// }
+// function execute(fn, name){
+//     fn();
+// }
+// execute(sayHello, 'Ankit');
+
+// function Morning(){
+//     console.log('Hello good morning');
+// }
+// function greet(){
+//     return Morning();
+// }
+
+
+function MorningGreet(){
+    console.log();
+    
+}
+function greet(){
+
+}
